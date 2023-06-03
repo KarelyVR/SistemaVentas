ec7f4d07d20d814746e3937c3eadcb6b5c72d36e
KarelyVR <karely_valdes07@live.com>
2023-06-02 23:00:31 -0600

    Revert "Merge branch 'master' of https://github.com/KarelyVR/SistemaVentas"
    This reverts commit 30a5aabbeb5f6236ec12b23016675ee4ae113353, reversing
    changes made to e06c37879a2e878e88e99b96a04c3d4751a710e8.

--- CapaPresentacion/Modales/mdAcercade.cs
+++ CapaPresentacion/Modales/mdAcercade.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CapaPresentacion.Modales
@@ -13,49 +20,6 @@ namespace CapaPresentacion.Modales
         private void label2_Click(object sender, EventArgs e)
         {
 
-        }
-
-        private void btndescargar_Click(object sender, EventArgs e)
-        {
-            // Ruta del archivo PDF en tu computadora
-            string rutaArchivo = @"C:\Users\fersa\OneDrive\Escritorio\SistemaVentas\SistemaVentas\CapaPresentacion\Resources\Manual de usuario.pdf";
-
-            // Verificar si el archivo existe
-            if (System.IO.File.Exists(rutaArchivo))
-            {
-                // Configurar la descarga del archivo
-                string nombreArchivo = System.IO.Path.GetFileName(rutaArchivo);
-
-                // Configurar el diálogo de descarga
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.FileName = nombreArchivo;
-                saveFileDialog.Filter = $"Archivos PDF (*.pdf)|*.pdf";
-                saveFileDialog.Title = "Guardar archivo PDF";
-                saveFileDialog.RestoreDirectory = true;
-
-                // Mostrar el diálogo de descarga
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    // Obtener la ruta de destino seleccionada por el usuario
-                    string rutaDestino = saveFileDialog.FileName;
-
-                    try
-                    {
-                        // Copiar el archivo al destino seleccionado
-                        System.IO.File.Copy(rutaArchivo, rutaDestino, true);
-
-                        MessageBox.Show("Archivo descargado correctamente.", "Descarga exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Ocurrió un error al descargar el archivo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show("El archivo no existe.", "Archivo no encontrado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
         }
     }
 }
